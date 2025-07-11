d50a770d6a4f0e5b718a7831ef43ee554d0927bb
ronaldewatts <ronaldewatts@gmail.com>
2025-07-10 21:13:27 -0500

    upload sample data

--- src/main/java/fiji/plugins/imageanalyzer/ImageAnalyzerMain.java
+++ src/main/java/fiji/plugins/imageanalyzer/ImageAnalyzerMain.java
@@ -2,6 +2,7 @@ package fiji.plugins.imageanalyzer;
 
 public class ImageAnalyzerMain {
     public static void main(String[] args) {
+        // To test, run this main and select the folder <project>/data/Cal3
         System.setProperty("ide", "true");
         new ImageAnalyzerPlugin().run();
     }
